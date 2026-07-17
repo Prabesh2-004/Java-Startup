f7910301ca67d1385c743b8f9fa73dd2234e2ff0
Prabesh2-004 <coder.bro2004@gmail.com>
2026-07-17 20:56:18 +0545

    basis types variable operators and typecasting learned

---
+++ src/Operators.java
@@ -0,0 +1,14 @@
+public class Operators {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 3;
+
+        double sum = a + b;
+        double mult = a * b;
+        double sub = a - b;
+        double div = (double) a / b;
+        
+
+        System.out.println(sum + " " + mult + " " + sub + " " + div);
+    }
+}
---
+++ src/VariableAndDataType.java
@@ -0,0 +1,23 @@
+
+public class VariableAndDataType {
+    public static void main (String[] args) {
+        int age = 21; // Stores whole numbers from -2,147,483,648 to 2,147,483,647
+
+        float length = 32.33f; // Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+
+        double temperature = 30.62d; // Stores fractional numbers. Sufficient for storing 15 to 16 decimal digits
+
+        char currencyType = '$'; // Stores a single character/letter or ASCII values
+
+        long netWorth = 12348565L; // stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+
+        short salary = 31232; // Stores whole numbers from -32,768 to 32,767
+
+        boolean isActive = true; // Stores true or false values
+
+        String name = "Frieren"; // A String variable contains a collection of characters surrounded by double quotes ("")
+
+        String[] cars = {"BMW", "Ford", "Ferrari"}; // Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
+        
+    }
+}
