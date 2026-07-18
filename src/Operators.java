cdef7e92db867dda59039410ba564137aa65d721
Prabesh2-004 <coder.bro2004@gmail.com>
2026-07-18 20:51:21 +0545

    operators and typecasting learned

--- src/Operators.java
+++ src/Operators.java
@@ -1,14 +1,51 @@
 public class Operators {
     public static void main(String[] args) {
-        int a = 6;
+        int a = 5;
         int b = 3;
 
-        double sum = a + b;
-        double mult = a * b;
-        double sub = a - b;
-        double div = (double) a / b;
-        
+        // Arithmetic Operators
+        int sum = a + b; //  add two variables
+        int mult = a * b; // multiply 2 variables
+        int sub = a - b; // subtract 2 variable
+        double div = (double) a / b; // divide numerator by denominator
+        int modulus = a % b; // get reminder after divide
+        int increment = 1;
+        increment++;
+        ++increment;
+        int decrement = 3;
+        decrement--;
+        --decrement;
 
-        System.out.println(sum + " " + mult + " " + sub + " " + div);
+        System.out.println(sum + " " + mult + " " + sub + " " + div + " " + modulus + " " + increment + " " + decrement);
+
+        // Different between post increment i++ vs pre increment ++i or same as decrement
+
+        int x = 3;
+        int y = ++x;
+        System.out.println(y); // output is 4, why? cause value of x increase before x value assign to y.
+
+        int m = 3;
+        int k = m++;
+        System.out.println(k); // output is 3, why? cause value increase after m assign to k so m already assign as 3 so it will become 3.
+
+
+        // Assignment Operators
+
+        int f = 5;
+        // Taking f value from parent where initialize not from previous value.
+//        f += 3; // output 8 cause f is 5 and adding by 3 become 8 it like f = f + 3;
+//        f -= 2; // output 3 cause f is 5 and subtracting by 2 become 3 it like f = f - 2;
+//        f *= 2; // output 10 cause f is 5 and multiplying by 2 become 10 it like f = f * 2;
+//        f /= 2; // output 2.5 cause f is 5 and dividing by 2 become 2.5 it like f = f / 2;
+//        f %= 3; // output 2 cause f is 5 which cannot directly divide by 3 so it will divide 3 and 2 become reminder;
+        f &= 3;
+//        f |= 2;
+//        f ^= 3;
+//        f >>= 3;
+//        f <<= 3;
+
+        System.out.println(f);
+
+
     }
 }
--- src/TypeCasting.java
+++ src/TypeCasting.java
@@ -1,3 +1,41 @@
 public class TypeCasting {
+    public static void main(String[] args) {
 
+        /*
+            There are 2 types of type casting
+            1. Widening Casting:
+                Widening casting is done automatically when passing a smaller size type into a larger size type.
+                This works because there is no risk of losing information. For example, an int value can safely fit inside a double.
+
+            2. Narrowing Casting:
+                Narrowing casting must be done manually by placing the type in parentheses () in front of the value.
+                This is required because narrowing may result in data loss (for example, dropping decimals when converting a double to an int).
+        */
+
+        //Widening Casting
+        int salary = 32445;
+        double doubleSalary = salary;
+        System.out.println(doubleSalary);
+
+        // Narrowing Casting
+        double myDouble = 9.78d;
+        int myInt = (int) myDouble;
+        System.out.println(myInt);
+
+
+        // Real World Example
+
+
+        // Set the maximum possible score in the game to 500
+        int maxScore = 500;
+
+        // The actual score of the user
+        int userScore = 423;
+
+        /* Calculate the percentage of the user's score in relation to the maximum available score.
+        Convert userScore to double to make sure that the division is accurate */
+        double percentage = (double) userScore / maxScore * 100.0d;
+
+        System.out.println("User's percentage is " + percentage);
+    }
 }
